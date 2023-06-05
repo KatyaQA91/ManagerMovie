5793ec73c2f3d317b1a1513da138fe6ac0b76175
KatyaQA <ekaterina.rus91@mail.ru>
2023-06-05 22:27:35 +0300

    first commit

---
+++ src/test/java/netology/ru/ManagerTest.java
@@ -0,0 +1,79 @@
+package netology.ru;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ManagerTest {
+    @Test
+    public void addingInListOfMovies() {  //добавление в список фильмов
+        Manager manager = new Manager();
+
+        manager.add("Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+
+
+        String[] expected = {"Film 1", "Film 2", "Film 3"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addingOneMovie() {  // Добавление одного филма
+        Manager manager = new Manager();
+
+        manager.add("Film 1");
+
+        String[] expected = {"Film 1"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void dontAddMovie() {  // Не добавлять фильм
+        Manager manager = new Manager();
+
+        String[] expected = {};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseMovieList() {  // Обратный список фильмов
+        Manager manager = new Manager();
+
+        manager.add("Film 5");
+        manager.add("Film 4");
+        manager.add("Film 3");
+        manager.add("Film 2");
+        manager.add("Film 1");
+
+        String[] expected = {"Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseListFromThreeMovies() { // добавление трех фильмов
+        Manager manager = new Manager();
+
+        manager.add("Film 1");
+
+        String[] expected = {"Film 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseListOfZeroMovies() { //добавление ноль
+        Manager manager = new Manager();
+
+
+        String[] expected = {};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
+
+
